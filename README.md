e183a825414c5074c1c753fd7241954a888a9bf4
Mykal Machon <mykalmachon@gmail.com>
2022-06-23 13:48:42 -0700

    initial upload 🎉

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This is a Serverless OG Image Generator. It's meant to work with Netlify Functions, but should work on any serverless platform you're using.
 
 The image generator uses HTML templates, renders them using Puppeteer, and then takes a screenshot that can be returned back to the user as a response. You should be able to scale this relatively easily as images are generated per-request, and can likely use cache-control headers to ensure efficient operation.
+
+## Testing Localy
+This isn't really production ready, as much as it is a proof of concept. 
+To run this locally you're going to need to set a few options in a .env file
+
+```
+NODE_ENV="development"
+PATH_TO_CHROME="/your/path/to/chrome"
+```
---
+++ index.ts
@@ -0,0 +1,41 @@
+import fs from "fs/promises";
+import puppeteer from "puppeteer-serverless";
+import 'dotenv/config';
+import * as nunjucks from "nunjucks";
+
+
+type HTMLType = "post" | "page";
+type HTMLContent = {
+  title: string;
+  description?: string;
+  image?: string;
+};
+
+const generateHTML = async (
+  type: HTMLType,
+  content: HTMLContent
+): Promise<string> => {
+  nunjucks.configure({ autoescape: true });
+  // get file needed
+  const fileText = await fs.readFile(`./templates/${type}.html`, {
+    encoding: "utf-8",
+  });
+  // generate the file as needed
+  const generatedHTML = nunjucks.renderString(fileText, content);
+  return generatedHTML;
+};
+
+const renderHTML = async (htmlCode: string): Promise<any> => {
+  const browser = await puppeteer.launch({executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'});
+  const page = await browser.newPage();
+  await page.setContent(htmlCode);
+  await page.setViewport({ width: 1200, height: 630 });
+  await page.screenshot({ path: "./screenshot.png" });
+};
+
+const run = async () => {
+  const html = await generateHTML("post", { title: "Hello World!" });
+  await renderHTML(html);
+};
+
+run();
